66f2fad0a28cb99a56f2ac2986ae8d2e8522bdfa
Walter Dunckel <radiohound@gmail.com>
2026-03-22 19:05:37 -0700

    debug add

--- scripts/lora_sdr_flowgraph.py
+++ scripts/lora_sdr_flowgraph.py
@@ -159,10 +159,10 @@ class LoRaSdrFlowgraph:
         deinterleaver  = lora_sdr.deinterleaver(False)
         hamming_dec    = lora_sdr.hamming_dec(False)
         header_decoder = lora_sdr.header_decoder(
-            False, cr, 255, False, 2, False
+            False, cr, 255, False, 2, True   # True = print header (debug)
         )
         dewhitening    = lora_sdr.dewhitening()
-        crc_verif      = lora_sdr.crc_verif(False, False)
+        crc_verif      = lora_sdr.crc_verif(True, False)  # True = print payload (debug)
 
         # --- Message sink: delivers decoded frames to Python ---
         msg_sink = _LoRaMessageSink(callback=self._on_packet)
@@ -254,6 +254,7 @@ class _LoRaMessageSink:
 
                 def _handle_msg(self_, msg):
                     try:
+                        log.debug("DEBUG _handle_msg: raw msg = %s", pmt.write_string(msg))
                         meta_pmt    = pmt.car(msg)
                         payload_pmt = pmt.cdr(msg)
                         payload = bytes(
